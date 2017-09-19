210785cb343e0ebec93fea0f0dee3bcf0a6945c5
donslovenkai <don.slovenkai@gmail.com>
2017-09-19 19:46:12 -0400

    updated html-routes.js

--- FriendFinder/app/routing/html-routes.js
+++ FriendFinder/app/routing/html-routes.js
@@ -10,7 +10,8 @@ module.exports = function(app){
   });
 
   //Default route to home page 
-  app.get('/', function (req, res) {
+
+  app.use(function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 };
